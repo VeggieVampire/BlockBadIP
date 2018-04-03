2fa64e99fdedc923fabd20919beb97725e3c9f9f
Nick Farrow <VeggieVampire@users.noreply.github.com>
2018-04-02 23:29:18 -0500

    Create block_bad_ips.bat

---
+++ block_bad_ips.bat
@@ -0,0 +1,66 @@
+@echo off
+setlocal enabledelayedexpansion
+echo "Download https://hosts-file.net/download/hosts.txt"
+echo "Save the text as blockit.txt in the same folder as this script."
+pause
+
+if "%1"=="list" (
+  SET /A RULECOUNT=0
+  for /f %%i in ('netsh advfirewall firewall show rule name^=all ^| findstr Blockit') do (
+    SET /A RULECOUNT+=1
+    netsh advfirewall firewall show rule Blockit!RULECOUNT! | findstr RemoteIP
+  )
+  SET "RULECOUNT="
+  exit/b
+)
+
+REM Deleting existing block on ips
+SET /A RULECOUNT=0
+for /f %%i in ('netsh advfirewall firewall show rule name^=all ^| findstr Blockit') do (
+  SET /A RULECOUNT+=1
+  netsh advfirewall firewall delete rule name="Blockit!RULECOUNT!"
+)
+SET "RULECOUNT="
+
+REM Block new ips (while reading them from blockit.txt)
+SET /A IPCOUNT=0
+SET /A BLOCKCOUNT=1
+for /f %%i in (blockit.txt) do (
+  SET /A IPCOUNT+=1
+  if !IPCOUNT! == 201 (
+    netsh advfirewall firewall add rule name="Blockit!BLOCKCOUNT!" protocol=any dir=in action=block 
+
+remoteip=!IPADDR!
+    netsh advfirewall firewall add rule name="Blockit!BLOCKCOUNT!" protocol=any dir=out action=block 
+
+remoteip=!IPADDR!
+    SET /A BLOCKCOUNT+=1
+    SET /A IPCOUNT=1
+    set IPADDR=%%i
+  ) else (
+    if not "!IPADDR!" == "" (  
+      set IPADDR=!IPADDR!,%%i
+    ) else (
+      set IPADDR=%%i
+    )
+  )
+)
+
+REM add the final block of IPs of length less than 200
+netsh advfirewall firewall add rule name="Blockit!BLOCKCOUNT!" protocol=any dir=in action=block 
+
+remoteip=!IPADDR!
+netsh advfirewall firewall add rule name="Blockit!BLOCKCOUNT!" protocol=any dir=out action=block 
+
+remoteip=!IPADDR!
+
+SET "IPCOUNT="
+SET "BLOCKCOUNT="
+SET "IPADDR="
+
+REM call this batch again with list to show the blocked IPs
+call %0 list
+
+echo "Found at https://serverfault.com/questions/653814/windows-firewall-netsh-block-all-ips-from-a-
+
+text-file"
